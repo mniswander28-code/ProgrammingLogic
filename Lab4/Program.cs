87d15b938b140a88591676736e4a1dc552fc764b
mniswander28-code <mniswander28@jcu.edu>
2026-07-27 14:39:03 +0000

    lab4 complete

---
+++ Lab4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class Car
+{
+    public string model;
+    public string color;
+    public int year;
+
+    public void Display()
+    {
+        Console.WriteLine("Car Details:");
+        Console.WriteLine("Model: " + model);
+        Console.WriteLine("Color: " + color);
+        Console.WriteLine("Year: " + year);
+    }
+
+    public void Start()
+    {
+        Console.WriteLine("The car is starting.");
+    }
+
+    public void Drive(int miles)
+    {
+        Console.WriteLine("The car drove " + miles + " miles.");
+    }
+
+    public string GetDescription()
+    {
+        return year + " " + color + " " + model;
+    }
+
+    public void Repaint(string newColor)
+    {
+        color = newColor;
+        Console.WriteLine("The car has been repainted to " + newColor + ".");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car myCar = new Car();
+        myCar.model = "Civic";
+        myCar.color = "Black";
+        myCar.year = 2020;
+
+        myCar.Display();
+
+        // Problem 1
+        myCar.Start();
+
+        // Problem 2
+        myCar.Drive(50);
+
+        // Problem 3
+        string description = myCar.GetDescription();
+        Console.WriteLine(description);
+
+        // Problem 4
+        myCar.Repaint("red");
+        Console.WriteLine("Updated color: " + myCar.color);
+    }
+}
