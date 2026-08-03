e981dfdebb70266b2d0e9af2ea6aaa5b97af2252
mniswander28-code <mniswander28@jcu.edu>
2026-08-03 20:19:19 +0000

    lab5 complete

---
+++ Lab5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void animalSound()
+    {
+        Console.WriteLine("The animal makes a sound");
+    }
+}
+
+class Cat : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The cat says: meow");
+    }
+}
+
+class Dog : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The dog says: bow wow");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal myAnimal = new Animal();
+        Animal myCat = new Cat();
+        Animal myDog = new Dog();
+
+        myAnimal.animalSound();
+        myCat.animalSound();
+        myDog.animalSound();
+    }
+}
